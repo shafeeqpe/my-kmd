a3dbe2e38b2fcd4c39f467bf545d1a87cc215259
shafeeqpe <shafeeqpem@gmail.com>
2018-11-21 19:08:52 +0530

    first commit

---
+++ precache-manifest.e57fc44c0fee2bbc1e00ac0a2c1f928d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "60cc4f66c159ea422072",
+    "url": "/static/js/main.60cc4f66.chunk.js"
+  },
+  {
+    "revision": "6c58186ad0ce379b81d5",
+    "url": "/static/js/1.6c58186a.chunk.js"
+  },
+  {
+    "revision": "60cc4f66c159ea422072",
+    "url": "/static/css/main.5dcd319d.chunk.css"
+  },
+  {
+    "revision": "6db0df0134487e286f1a89cf444d0230",
+    "url": "/index.html"
+  }
+];
